d49600fb4df177352c2b80a513317abbef1467b7
Payal <payal.mittal2311@gmail.com>
2021-03-31 23:14:10 +0530

    Second Java Program changes

--- program_2.java
+++ program_2.java
@@ -1,7 +1,9 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
 class Addition {
     public static void main(String[] args) {
-      int Fnum1, Snum2, sum;
+      int Fnum1, Snum2, sum,sub,mult,div,rem;
         Scanner sc = new Scanner(System.in);
         System.out.println("First Number: ");
         Fnum1 = sc.nextInt();
@@ -10,5 +12,13 @@ class Addition {
         sc.close();
         sum = Fnum1 + Snum2;
         System.out.println("Sum of these numbers: "+sum);
+        sub = Fnum1 - Snum2;
+        System.out.println("Subtraction of these numbers: "+sub);
+        mult = Fnum1 * Snum2;
+        System.out.println("Multiplication of these numbers: "+mult);
+        rem = Fnum1 % Snum2;
+        System.out.println("Remainder of these numbers: "+rem);
+        div = Fnum1 / Snum2;
+        System.out.println("Division of these numbers: "+div);
     }
 }
