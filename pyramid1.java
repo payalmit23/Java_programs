daa681c0f366acf73dba21415e45f3f2c6f13746
Payal <payal.mittal2311@gmail.com>
2021-04-07 21:51:01 +0530

    Pyramid

---
+++ pyramid1.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main(String[] args) {
+       int i,j,n=5;
+        for(i=0;i<n;i++){
+			for(j=n-i;j>0;j--){
+				System.out.print(" ");
+			}
+			for(j=0;j<=i;j++){
+				System.out.print("*"); 
+			}
+        System.out.println();
+		}
+    }
+}
+
+   *
+  **
+ ***
+****
+
+
